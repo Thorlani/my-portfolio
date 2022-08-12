c611c2aa3737c26d3885a0464d207a4e8e07e044
Omotolani Popoola <omotolanipopoola@DotPays-MacBook-Pro-2.local>
2022-08-12 18:44:18 +0100

    final push.lol

--- script.js
+++ script.js
@@ -1,7 +1,66 @@
- const menuToggle = document.querySelector('.toggle')
- const showcase = document.querySelector('.showcase')
+const menuToggle = document.querySelector('.toggle')
+const showcase = document.querySelector('.showcase')
 
- menuToggle.addEventListener('click', () => {
-     menuToggle.classList.toggle('active')
-     showcase.classList.toggle('active')
- })
+menuToggle.addEventListener('click', () => {
+    menuToggle.classList.toggle('active')
+    showcase.classList.toggle('active')
+})
+
+const show = document.querySelector('.show')
+const hide = document.querySelector('.full')
+const background = document.querySelector('.background')
+
+show.addEventListener('click', () => {
+    background.classList.add('active')
+})
+
+hide.addEventListener('click', () => {
+    background.classList.remove('active')
+})
+
+window.addEventListener("DOMContentLoaded", function () {
+    // get the form elements defined in your form HTML above
+  
+    var form = document.getElementById("my-form");
+    // var button = document.getElementById("my-form-button");
+    var status = document.getElementById("status");
+  
+    // Success and Error functions for after the form is submitted
+  
+    function success() {
+      form.reset();
+      status.classList.add("success");
+      status.innerHTML = "Thanks! Your form has been submitted.";
+    }
+  
+    function error() {
+      status.classList.add("error");
+      status.innerHTML = "Oops! There was a problem.";
+    }
+  
+    // handle the form submission event
+  
+    form.addEventListener("submit", function (ev) {
+      ev.preventDefault();
+      var data = new FormData(form);
+      ajax(form.method, form.action, data, success, error);
+    });
+  });
+  
+  // helper function for sending an AJAX request
+  
+  function ajax(method, url, data, success, error) {
+    var xhr = new XMLHttpRequest();
+    xhr.open(method, url);
+    xhr.setRequestHeader("Accept", "application/json");
+    xhr.onreadystatechange = function () {
+      if (xhr.readyState !== XMLHttpRequest.DONE) return;
+      if (xhr.status === 200) {
+        success(xhr.response, xhr.responseType);
+      } else {
+        error(xhr.status, xhr.response, xhr.responseType);
+      }
+    };
+    xhr.send(data);
+  }
+  
